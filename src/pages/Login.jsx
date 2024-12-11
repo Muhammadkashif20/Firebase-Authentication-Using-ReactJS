ba3b769765525886cf91f1d85b6855f1f3305dfc
muhammadkashif20 <kashif28@gmail.com>
2024-12-11 16:11:34 +0500

    UI working based Login & SignUp With NextUi Library

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,33 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import {Form, Input, Button} from "@nextui-org/react";
-import { div, h1 } from "framer-motion/client";
 export default function Login() {
+    const [email,setEmail]=useState("")
+    const [password,setPassword]=useState("")
   return (
     <div>
-        <h1 className="font-bold text-center text-2xl my-3">Login </h1>
-    <Form validationBehavior="native" className="my-3 flex flex-col items-center">
+        <h1 className="font-bold text-center text-2xl my-5">Login </h1>
+    <Form validationBehavior="native" className="my-5 flex flex-col items-center">
       <Input
         isRequired
         label="Email"
         labelPlacement="outside"
         name="Email"
+        value={email}
+        onChange={(e)=>setEmail(e.target.value)}
         placeholder="Enter your Email"
         type="text"
         size="lg"
-        className="w-1/2 my-3 sm:full"
+        className="w-1/3 my-5 sm:full"
       />
        <Input
         isRequired
         label="Password"
         labelPlacement="outside"
         name="Password"
+        value={password}
+        onChange={(e)=>setPassword(e.target.value)}
         placeholder="Enter your Password"
         type="text"
         size="lg"
-        className="w-1/2 my-3 sm:full"
+        className="w-1/3 my-5 sm:full"
         />
-      <Button className="w-1/2" type="submit" variant="bordered">
+      <Button className="w-1/3" type="submit" variant="bordered">
         Login
+      </Button>
+      <h1 className="font-bold text-center ">OR</h1>
+      <Button className="w-1/3" type="submit" variant="bordered">
+        Login with Google
       </Button>
     </Form>
         </div>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
-const Signup = () => {
+import { useState } from "react";
+import React from "react";
+import {Form, Input, Button} from "@nextui-org/react";
+export default function Signup() {
+    const [username,setUsername]=useState()
+    const [email,setEmail]=useState()
+    const [password,setPassword]=useState()
   return (
     <div>
-      <h1 className='font-bold text-center text-2xl'>SignUp</h1>
-    </div>
-  )
+        <h1 className="font-bold text-center text-2xl my-5">SignUp </h1>
+    <Form validationBehavior="native" className="my-5 flex flex-col items-center">
+       <Input
+        isRequired
+        label="Username"
+        labelPlacement="outside"
+        name="Username"
+        value={username}
+        onChange={(e)=>setUsername(e.target.value)}
+        placeholder="Enter your Username"
+        type="text"
+        size="lg"
+        className="w-1/3 my-5 sm:full"
+        />
+         <Input
+        isRequired
+        label="Email"
+        labelPlacement="outside"
+        name="Email"
+        value={email}
+        onChange={(e)=>setEmail(e.target.value)}
+        placeholder="Enter your Email"
+        type="text"
+        size="lg"
+        className="w-1/3 my-5 sm:full"
+      />
+         <Input
+        isRequired
+        label="Password"
+        labelPlacement="outside"
+        name="Password"
+        value={password}
+        onChange={(e)=>setPassword(e.target.value)}
+        placeholder="Enter your Password"
+        type="text"
+        size="lg"
+        className="w-1/3 my-5 sm:full"
+        />
+      <Button className="w-1/3" type="submit" variant="bordered">
+      SignUp
+      </Button>
+    </Form>
+        </div>
+  );
 }
-export default Signup
